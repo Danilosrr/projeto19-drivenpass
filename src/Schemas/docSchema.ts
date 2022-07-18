6260b2a288123e0d3ae3449fea9975a63bee8e08
Danilo <danilo_srr@hotmail.com>
2022-07-17 22:55:23 -0300

    doc schema fix

--- src/Schemas/docSchema.ts
+++ src/Schemas/docSchema.ts
@@ -3,7 +3,7 @@ import { bodyDoc } from "../Repositories/docRepository.js";
 
 export const docSchema = joi.object<bodyDoc>({
     title: joi.string().required(),
-    number: joi.string().creditCard().required(),
+    number: joi.string().required(),
     completeName: joi.string().required(),
     issuingBody: joi.string().required(),
     emissionDate: joi.string().pattern(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/).required().messages({"string.pattern.base":"Emission date format: MM/YY"}),
