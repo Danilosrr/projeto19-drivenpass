914861336e0a47e118874d2083a624e01d46e661
Luana Lessa <lessalsn@gmail.com>
2022-08-02 13:19:36 -0300

    Code review

--- src/Schemas/userSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi"
-import { createUser } from "../Repositories/userRepository.js";
-
-export const userSchema = joi.object<createUser>({
-    email: joi.string().email().required(),
-    password: joi.string().required().min(10),
-});
